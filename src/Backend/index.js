bd35902df3130d7f5aa869fbd3e8fcbcb240e6eb
Triumph <realTriumphNdlovu@gmail.com>
2025-03-10 11:16:38 +0200

    Add helmet middleware for enhanced security and update package dependencies

--- src/Backend/index.js
+++ src/Backend/index.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const helmet = require('helmet');
 dotenv.config();
 
 const app = express();
 const cors = require('cors');
 app.use(cors());
 app.use(express.json()); // to parse JSON bodies
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'"],
+        imgSrc: ["'self'", 'https://jobman-tffe.onrender.com'],
+        connectSrc: ["'self'", 'https://generativelanguage.googleapis.com'],
+      },
+    },
+  })
+);
+
 
 const PORT = process.env.PORT || 5000;
 
