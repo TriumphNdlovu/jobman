b45128de017eb70918171487ba796d9fa13194bf
Triumph <realTriumphNdlovu@gmail.com>
2025-03-17 10:56:00 +0200

    Add CV Review and Home Home components with initial templates and styles added new color schemes

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -2,10 +2,14 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ResultsComponent } from './results/results.component';
 import { LandingComponent } from './landing/landing.component';
+import { HomeHomeComponent } from './home-home/home-home.component';
+import { CvReviewComponent } from './cv-review/cv-review.component';
 
 export const routes: Routes = [
   { path: '', component: LandingComponent},
   { path: 'home', component: HomeComponent },
   { path: 'results', component: ResultsComponent },
+  { path: 'home-home', component: HomeHomeComponent },
+  { path: 'cv-review', component: CvReviewComponent },
   { path: '**', redirectTo: '' }
 ];
---
+++ Frontend/src/app/cv-review/cv-review.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cv-review',
+  imports: [],
+  templateUrl: './cv-review.component.html',
+  styleUrl: './cv-review.component.css'
+})
+export class CvReviewComponent {
+
+}
