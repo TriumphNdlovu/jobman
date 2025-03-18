c0ecc380be09b3ab10601ded850ad8ce074dc469
Triumph <realTriumphNdlovu@gmail.com>
2025-03-18 16:15:41 +0200

    batman

--- Frontend/src/app/cv-review/cv-review.component.ts
+++ Frontend/src/app/cv-review/cv-review.component.ts
@@ -37,7 +37,7 @@ export class CvReviewComponent {
   const formData = new FormData();
   formData.append("cv", this.selectedFile);
   this.loading = true;
-  fetch("http://localhost:5000/cvupload", {
+  fetch("https://jobman-tfev.onrender.com/cvupload", {
 
     method: "POST",
     body: formData
